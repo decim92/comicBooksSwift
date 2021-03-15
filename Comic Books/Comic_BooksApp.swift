5df78ada8f9b64c487ef6edd85195535652fad9f
Douglas Poveda <douglas.poveda@sundevs.com>
2021-03-15 17:05:56 -0500

    Initial commit

---
+++ Comic Books/Comic_BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comic_BooksApp.swift
+//  Comic Books
+//
+//  Created by Douglas Poveda on 15/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Comic_BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-Comic Books/Pods-Comic Books-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Comic_Books : NSObject
+@end
+@implementation PodsDummy_Pods_Comic_Books
+@end
